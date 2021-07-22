a5510304c3600d8c3c6224676416813d3d1a568a
gloire lutete <gloirelutete@gloires-Air.lan>
2021-07-22 13:27:54 -0400

    first commit

---
+++ src/app/buy-home/buy-home.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buy-home',
+  templateUrl: './buy-home.component.html',
+  styleUrls: ['./buy-home.component.css']
+})
+export class BuyHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
---
+++ src/app/sell-home/sell-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sell-home',
+  templateUrl: './sell-home.component.html',
+  styleUrls: ['./sell-home.component.css']
+})
+export class SellHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
